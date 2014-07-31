4d8fd06420346dc5a59c87bf4e9adc0dcf5d7dc6
Manny Lozada <mannylozada@icloud.com>
2014-07-30 23:54:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EscapeAndRenew::Application.config.secret_key_base = '9c3f541706c275a14ef166dcf3897e73e75d7c3cd72229b603d5a429f5249ed5347472afe55b81a5af07a59ea8d45c7bb4def285a89d270269874ec602608b41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EscapeAndRenew::Application.config.session_store :cookie_store, key: '_escape_and_renew_session'
